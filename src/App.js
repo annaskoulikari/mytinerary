0beea938f67395fd0aaaa460199d14bebc24a6c7
Anna Skoulikari <annaskoulikari@gmail.com>
2018-12-26 13:54:08 +0100

    third-design created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import LandingPageDuplicate from "./components/landingPageDuplicate";
+import LandingPage from "./components/landingPage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CitiesPage from "./components/citiesPage";
 import LoginPage from "./components/loginPage";
 import SignupPage from "./components/signupPage";
 import MenuOpen from "./components/menuOpen";
+import Footer from "./components/footer";
 
 class App extends Component {
   render() {
@@ -13,13 +14,14 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Switch>
-            <Route exact path="/" component={LandingPageDuplicate} />
+            <Route exact path="/" component={LandingPage} />
             <Route path="/citiesPage" component={CitiesPage} />
             <Route path="/loginPage" component={LoginPage} />
             <Route path="/signupPage" component={SignupPage} />
             <Route path="/menuOpen" component={MenuOpen} />
             <Route render={() => <h3>Oops no page</h3>} />
           </Switch>
+          <Footer />
         </div>
       </BrowserRouter>
     );
--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -3,12 +3,14 @@ import logotest from "../MYtineraryLogo.png";
 import cities from "../circled-right-2.png";
 import "../App.css";
 import { NavLink } from "react-router-dom";
+import Header from "./header";
 
 class LangingPage extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
+        <Header />
         <div className="row landingPageContents">
           <img className="logo" src={logotest} alt="logo" />
           <p className="landingPageText">
