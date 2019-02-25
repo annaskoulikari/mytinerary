d5c4af49d230e6b51f9067e301efd581361160a9
Anna Skoulikari <annaskoulikari@gmail.com>
2019-02-26 00:08:12 +0100

    added a back button

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,15 +3,65 @@ import home from "../homeIcon.png";
 import "../App.css";
 import { NavLink } from "react-router-dom";
 
+import ArrowLeft from "@material-ui/icons/ArrowLeft";
+import IconButton from "@material-ui/core/IconButton";
+
+import { createBrowserHistory } from "history";
+
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      homePage: false
+    };
+  }
+
   render() {
+    const history = createBrowserHistory();
     return (
       <div className="bottomBar">
-        <div className="footerBar">
-          <NavLink to="/">
-            <img className="footerBarHome" src={home} alt="home logo" />
-          </NavLink>
-        </div>
+        {history.location.pathname === "/" ? (
+          <div>
+            <div>
+              {" "}
+              <div className="footerBar">
+                <NavLink to="/">
+                  <img className="footerBarHome" src={home} alt="home logo" />
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            {" "}
+            <div className="footerBar">
+              <div
+                style={{
+                  flex: 1,
+                  display: "flex",
+                  justifyContent: "flexStart"
+                }}
+              >
+                <IconButton
+                  onClick={() => {
+                    history.goBack();
+                  }}
+                >
+                  <ArrowLeft style={{ fontSize: 50, color: "black" }} />
+                </IconButton>
+              </div>
+              <div
+                style={{ flex: 1, display: "flex", justifyContent: "center" }}
+              >
+                <NavLink to="/">
+                  <img className="footerBarHome" src={home} alt="home logo" />
+                </NavLink>
+              </div>
+              <div style={{ flex: 1 }} />
+            </div>
+          </div>
+        )}
       </div>
     );
   }
