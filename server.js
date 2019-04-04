ea3f66a0dc898c83c9c95f470db134e00aa194c7
Anna Skoulikari <annaskoulikari@gmail.com>
2019-04-04 14:39:45 +0200

    cleaned up server.js file and package.json - code refactor

--- server.js
+++ server.js
@@ -1,43 +1,19 @@
-var routerTest = require("./routes/test");
-var routerItinerary = require("./routes/itineraryRoute");
-var routerActivity = require("./routes/activityRoute");
-var routerComment = require("./routes/commentRoute");
-var routerCountry = require("./routes/countryRoute");
-var routerAccount = require("./routes/accountRoute");
-var routerLogin = require("./routes/loginRoute");
-var routerAuth = require("./routes/authRoute");
-
-var routerProfile = require("./routes/profileRoute");
-//var routerPhoto = require("./routes/photoRoute");
-
-var favouriteRoute = require("./routes/favouriteRoute");
 require("dotenv").config();
 var cors = require("cors");
 var passport = require("passport");
-
 const path = require("path");
 const crypto = require("crypto");
 const multer = require("multer");
 const GridFsStorage = require("multer-gridfs-storage");
 const Grid = require("gridfs-stream");
 const methodOverride = require("method-override");
-const fs = require("fs");
-//trying to take out https for heroku
 
-// var https = require("https");
 var http = require("http");
 
 var express = require("express");
 
-// const certOptions = {
-//   key: fs.readFileSync(path.resolve("./ssl/server.key")),
-//   cert: fs.readFileSync(path.resolve("./ssl/server.crt"))
-// };
 var app = express();
 
-//trying to take out https for heroku
-
-// const server = https.createServer(certOptions, app);
 const server = http.createServer(app);
 
 var port = process.env.PORT || 5000;
@@ -45,26 +21,11 @@ server.listen(port, () => console.log(`server running on port ${port}`));
 
 const session = require("express-session");
 
-console.log("server is starting");
-
 var mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
-var bodyParser = require("body-parser");
-
-require("dotenv").config();
-
-const socketio = require("socket.io");
-
-// const server = http.createServer(app);
-//const server = http.Server(app);
-
-const io = socketio.listen(server);
-
-io.sockets.on("connection", function(socket) {
-  console.log("A client is connected!");
-});
 
 //middleware
+var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
@@ -87,18 +48,9 @@ app.use(
   })
 );
 
-// var io = require("socket.io").listen(server);
-
-// console.log("this is io", io);
-// console.log("this is io id", io.id);
-// const io = socketio.listen(server);
-app.set("io", io);
-
-console.log("this is app", app.io);
-
 app.use(express.static("uploads"));
 app.use("/uploads", express.static("uploads"));
-app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.db_url, {
@@ -138,15 +90,8 @@ const storage = new GridFsStorage({
 const upload = multer({ storage });
 
 app.post("/uploads", upload.single("file"), (req, res) => {
-  console.log("we reached upload backend route");
   res.send("yup we got your image upload");
-  // res.json({ file: req.file });
 });
-
-// app.post("/uploads", (req, res) => {
-//   console.log("we reached upload backend route");
-//   // res.json({ file: req.file });
-// });
 
 mongoose.Promise = global.Promise;
 
@@ -165,17 +110,16 @@ app.get("/test", function(req, res) {
   res.send("Hello World");
 });
 
-app.use("/testRouter", routerTest);
-app.use("/testItinerary", routerItinerary);
-app.use("/testActivity", routerActivity);
-app.use("/testComment", routerComment);
-app.use("/testCountry", routerCountry);
-app.use("/testAccount", routerAccount);
-app.use("/testLogin", routerLogin);
-app.use("/testProfile", routerProfile);
-app.use("/auth", routerAuth);
-//app.use("/testUpload", routerPhoto);
-app.use("/favourite", favouriteRoute);
+app.use("/testRouter", require("./routes/test"));
+app.use("/testItinerary", require("./routes/itineraryRoute"));
+app.use("/testActivity", require("./routes/activityRoute"));
+app.use("/testComment", require("./routes/commentRoute"));
+app.use("/testCountry", require("./routes/countryRoute"));
+app.use("/testAccount", require("./routes/accountRoute"));
+app.use("/testLogin", require("./routes/loginRoute"));
+app.use("/testProfile", require("./routes/profileRoute"));
+app.use("/auth", require("./routes/authRoute"));
+app.use("/favourite", require("./routes/favouriteRoute"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
